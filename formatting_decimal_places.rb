abcce5494dbb5f0e2dea3e7b319fbfd8ce97142c
Nick Pisciotta <npisciotta@gmail.com>
2016-12-06 11:28:47 -0700

    Complete formatting decimal exercise

---
+++ formatting_decimal_places.rb
@@ -0,0 +1,10 @@
+def two_decimal_places(number)
+  characteristic, mantissa = number.to_s.split(".")
+  [characteristic, mantissa[0..1]].join('.').to_f
+end
+
+puts two_decimal_places(10.1289767789) # => 10.12
+
+# def two_decimal_places(number)
+#   (number * 100).to_i/100.00
+# end
